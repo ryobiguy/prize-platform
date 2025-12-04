bbf4dc7a8f0bce1cd06d24ac4ef9d331474caebd
Prize Platform <user@prizeplatform.com>
2025-12-04 19:52:03 +0000

    fix-auth-middleware-destructuring

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const User = require('../models/User');
 const TaskCompletion = require('../models/TaskCompletion');
 const Prize = require('../models/Prize');
