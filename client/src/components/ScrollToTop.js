455e3dfda7f74ecb082076bfb52abfcd4c2bb665
Prize Platform <user@prizeplatform.com>
2025-12-09 20:32:54 +0000

    make scroll to top more aggressive with multiple scroll methods

--- client/src/components/ScrollToTop.js
+++ client/src/components/ScrollToTop.js
@@ -5,7 +5,16 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // Scroll to top immediately
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'instant'
+    });
+    
+    // Also try document scroll
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
   }, [pathname]);
 
   return null;
